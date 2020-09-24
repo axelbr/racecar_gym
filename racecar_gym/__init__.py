46396a5d095098282c5bc2a5cfcefb399a8db4ed
axelbr <axel.brunnbauer@gmx.at>
2020-09-24 13:49:47 +0200

    cleanup

--- racecar_gym/__init__.py
+++ racecar_gym/__init__.py
@@ -1,7 +1,5 @@
 import os
-
 from gym.envs.registration import register
-
 from racecar_gym.envs.scenarios import MultiAgentScenario
 
 base_path = os.path.dirname(__file__)
@@ -22,28 +20,4 @@ register(id='austria-four-v0',
                  path=f'{base_path}/../scenarios/austria.yml',
                  rendering=False
              )
-         })
-
-register(id='f1tenth-berlin-two-gui-v0',
-         entry_point='racecar_gym.envs.multi_race_car_env:MultiAgentRaceCarEnv',
-         kwargs={
-             'scenario': MultiAgentScenario.from_spec(f'{base_path}/../scenarios/berlin_two_agents_gui.yml')
-         })
-
-register(id='f1tenth-berlin-two-v0',
-         entry_point='racecar_gym.envs.multi_race_car_env:MultiAgentRaceCarEnv',
-         kwargs={
-             'scenario': MultiAgentScenario.from_spec(f'{base_path}/../scenarios/berlin_two_agents.yml')
-         })
-
-register(id='f1tenth-porto-two-gui-v0',
-         entry_point='racecar_gym.envs.multi_race_car_env:MultiAgentRaceCarEnv',
-         kwargs={
-             'scenario': MultiAgentScenario.from_spec(f'{base_path}/../scenarios/porto_two_agents_gui.yml')
-         })
-
-register(id='f1tenth-porto-two-v0',
-         entry_point='racecar_gym.envs.multi_race_car_env:MultiAgentRaceCarEnv',
-         kwargs={
-             'scenario': MultiAgentScenario.from_spec(f'{base_path}/../scenarios/porto_two_agents.yml')
          })
