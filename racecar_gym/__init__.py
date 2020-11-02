c5a21426c252e027c37a0395326a6d4d7f816c9e
axelbr <axel.brunnbauer@gmx.at>
2020-11-02 15:21:29 +0100

    expose registry

--- racecar_gym/__init__.py
+++ racecar_gym/__init__.py
@@ -1,8 +1,7 @@
 import os
-
 from gym.envs.registration import register
-
 from .envs import MultiAgentRaceEnv, SingleAgentRaceEnv, MultiAgentScenario, SingleAgentScenario
+from .tasks import get_task, register_task, Task
 
 base_path = os.path.dirname(__file__)
 
