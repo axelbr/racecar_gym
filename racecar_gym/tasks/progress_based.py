8bbca55efe6b5cf6bf22cbb8b21f0c1a2c5d9873
luigiberducci <luigi.berd@gmail.com>
2020-11-12 16:08:03 +0100

    add speed in reward def

--- racecar_gym/tasks/progress_based.py
+++ racecar_gym/tasks/progress_based.py
@@ -1,86 +1,9 @@
 from .task import Task
-
-class MaximizeContinuousProgressTask(Task):
-  def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool,
-               collision_reward: float = 0, frame_reward: float = 0, progress_factor: float = +100):
-    self._laps = laps
-    self._time_limit = time_limit
-    self._terminate_on_collision = terminate_on_collision
-    self._last_progress = None
-    self._collision_reward = collision_reward
-    self._frame_reward = frame_reward
-    self._progress_factor = progress_factor
-
-  def reward(self, agent_id, state, action) -> float:
-    agent_state = state[agent_id]
-    progress = agent_state['lap'] + agent_state['progress']
-    if self._last_progress is None:
-      self._last_progress = progress
-    delta = progress - self._last_progress
-    if delta > .5:  # the agent is crossing the starting line in the wrong direction
-      delta = -delta
-    reward = self._frame_reward
-    collision = agent_state['wall_collision'] or len(agent_state['opponent_collisions']) > 0
-    if collision:
-      reward += self._collision_reward
-    else:
-      reward += self._progress_factor * delta
-      self._last_progress = progress
-    return reward
-
-  def done(self, agent_id, state) -> bool:
-    agent_state = state[agent_id]
-    collision = agent_state['wall_collision'] or len(agent_state['opponent_collisions']) > 0
-    if self._terminate_on_collision and collision:
-      return True
-    return agent_state['lap'] > self._laps or self._time_limit < agent_state['time']
-
-  def reset(self):
-    self._last_progress = None
-
+import numpy as np
 
 class MaximizeProgressTask(Task):
-    def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool,
-                 delta_progress=0.001, collision_reward=-100, frame_reward=-0.1, progress_reward=1):
-        self._time_limit = time_limit
-        self._laps = laps
-        self._terminate_on_collision = terminate_on_collision
-        self._last_stored_progress = None
-        # reward params
-        self._delta_progress = delta_progress
-        self._progress_reward = progress_reward
-        self._collision_reward = collision_reward
-        self._frame_reward = frame_reward
-
-
-    def reward(self, agent_id, state, action) -> float:
-        agent_state = state[agent_id]
-        progress = agent_state['lap'] + agent_state['progress']
-        if self._last_stored_progress is None:
-            self._last_stored_progress = progress
-        delta = progress - self._last_stored_progress
-        reward = self._frame_reward
-        collision = agent_state['wall_collision'] or len(agent_state['opponent_collisions']) > 0
-        if collision == True:
-            reward += self._collision_reward
-        reward += delta * 10.0
-        self._last_stored_progress = progress
-        return reward
-
-    def done(self, agent_id, state) -> bool:
-        agent_state = state[agent_id]
-        collision = agent_state['wall_collision'] or len(agent_state['opponent_collisions']) > 0
-        if self._terminate_on_collision and collision:
-            return True
-        return agent_state['lap'] > self._laps or self._time_limit < agent_state['time']
-
-    def reset(self):
-        self._last_stored_progress = None
-
-
-class MaximizeDiscreteProgressTask(Task):
   def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool,
-               delta_progress=0.0, collision_reward=0, frame_reward=0.0, progress_reward=1):
+               delta_progress=0.0, collision_reward=0, frame_reward=0, progress_reward=100):
     self._time_limit = time_limit
     self._laps = laps
     self._terminate_on_collision = terminate_on_collision
@@ -103,9 +26,8 @@ class MaximizeDiscreteProgressTask(Task):
     collision = agent_state['wall_collision'] or len(agent_state['opponent_collisions']) > 0
     if collision == True:
       reward += self._collision_reward
-    elif delta > self._delta_progress:
-      reward += self._progress_reward
-      self._last_stored_progress = progress
+    reward += delta * self._progress_reward
+    self._last_stored_progress = progress
     return reward
 
   def done(self, agent_id, state) -> bool:
@@ -118,15 +40,28 @@ class MaximizeDiscreteProgressTask(Task):
   def reset(self):
     self._last_stored_progress = None
 
+
+class MaximizeFastProgressTask(MaximizeProgressTask):
+  def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool, delta_progress=0.001,
+               collision_reward=0, frame_reward=0, progress_reward=100, velocity_reward=0.01):
+    super().__init__(laps, time_limit, terminate_on_collision, delta_progress, collision_reward, frame_reward,
+                     progress_reward)
+    self._velocity_reward = velocity_reward
+
+  def reward(self, agent_id, state, action) -> float:
+    progress_reward = super().reward(agent_id, state, action)
+    velocity_reward = self._velocity_reward * np.linalg.norm(state[agent_id]['velocity'])
+    return progress_reward + velocity_reward
+
+
 class RankDiscountedMaximizeProgressTask(MaximizeProgressTask):
+  def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool, delta_progress=0.001,
+               collision_reward=-100, frame_reward=-0.1, progress_reward=1):
+    super().__init__(laps, time_limit, terminate_on_collision, delta_progress, collision_reward, frame_reward,
+                     progress_reward)
 
-    def __init__(self, laps: int, time_limit: float, terminate_on_collision: bool, delta_progress=0.001,
-                 collision_reward=-100, frame_reward=-0.1, progress_reward=1):
-        super().__init__(laps, time_limit, terminate_on_collision, delta_progress, collision_reward, frame_reward,
-                         progress_reward)
-
-    def reward(self, agent_id, state, action) -> float:
-        rank = state[agent_id]['rank']
-        reward = super().reward(agent_id, state, action)
-        reward = reward / float(rank)
-        return reward
+  def reward(self, agent_id, state, action) -> float:
+    rank = state[agent_id]['rank']
+    reward = super().reward(agent_id, state, action)
+    reward = reward / float(rank)
+    return reward
