6fccacbd6cae6080edcbf4497dfaf007cbcef7f3
axelbr <axel.brunnbauer@gmx.at>
2020-11-30 12:13:06 +0100

    added new track

--- examples/simple_usage.py
+++ examples/simple_usage.py
@@ -2,7 +2,7 @@ from time import sleep
 import gym
 from racecar_gym.envs import MultiAgentRaceEnv
 
-env = gym.make('MultiAgentBerlin_Gui-v0')
+env = gym.make('SingleAgentPlechaty_Gui-v0')
 
 done = False
 
@@ -14,13 +14,14 @@ t = 0
 while not done:
     action = env.action_space.sample()
     obs, rewards, dones, states = env.step(action)
-    done = any(dones.values())
+    print(states['progress'])
+    done = done#any(dones.values())
     sleep(0.01)
     if t % 100 == 0:
         # Currently, two rendering modes are available: 'birds_eye' and 'follow'
         # birds_eye: Follow an agent in birds eye perspective.
         # follow: Follow an agent in a 3rd person view.
-        image = env.render(mode='birds_eye', agent='A')
+        image = env.render(mode='birds_eye')
     t+=1
 
 
--- racecar_gym/__init__.py
+++ racecar_gym/__init__.py
@@ -5,29 +5,28 @@ from .tasks import get_task, register_task, Task
 
 base_path = os.path.dirname(__file__)
 
-def _register(name: str, file: str, is_multi_agent: bool, rendering: bool):
-    if is_multi_agent:
-        scenario = MultiAgentScenario.from_spec(path=f'{base_path}/../scenarios/{file}', rendering=rendering)
-        entry_point = 'racecar_gym.envs:MultiAgentRaceEnv'
-    else:
-        scenario = SingleAgentScenario.from_spec(path=f'{base_path}/../scenarios/{file}', rendering=rendering)
-        entry_point = 'racecar_gym.envs:SingleAgentRaceEnv'
+def _register_multi_agent(name: str, file: str, rendering: bool):
+    scenario = MultiAgentScenario.from_spec(path=f'{base_path}/../scenarios/{file}', rendering=rendering)
     register(
         id=name,
-        entry_point=entry_point,
+        entry_point='racecar_gym.envs:MultiAgentRaceEnv',
+        kwargs={'scenario': scenario}
+    )
+
+def _register_single_agent(name: str, file: str, rendering: bool):
+    scenario = SingleAgentScenario.from_spec(path=f'{base_path}/../scenarios/{file}', rendering=rendering)
+    register(
+        id=name,
+        entry_point='racecar_gym.envs:SingleAgentRaceEnv',
         kwargs={'scenario': scenario}
     )
 
 for scenario_file in os.listdir(f'{base_path}/../scenarios'):
-    type_track = os.path.basename(scenario_file).split('.')[0].split("_", 1)[0]    # `single` or `multi`
-    track_name = os.path.basename(scenario_file).split('.')[0].split("_", 1)[1]    # `austria`, `berlin`, ...
-    if type_track == "single":
-        name = f'SingleAgent{track_name.capitalize()}'
-        is_multi_agent = False
-    elif type_track == "multi":
-        name = f'MultiAgent{track_name.capitalize()}'
-        is_multi_agent = True
-    else:
-        raise NotImplementedError(f"type track {type_track} not supported")
-    _register(name=f'{name}-v0', file=scenario_file, is_multi_agent=is_multi_agent, rendering=False)
-    _register(name=f'{name}_Gui-v0', file=scenario_file, is_multi_agent=is_multi_agent, rendering=True)
+    track_name = os.path.basename(scenario_file).split('.')[0]
+    name = f'{track_name.capitalize()}'
+    _register_multi_agent(name=f'MultiAgent{name}-v0', file=scenario_file, rendering=False)
+    _register_multi_agent(name=f'MultiAgent{name}_Gui-v0', file=scenario_file, rendering=True)
+    _register_single_agent(name=f'SingleAgent{name}-v0', file=scenario_file, rendering=False)
+    _register_single_agent(name=f'SingleAgent{name}_Gui-v0', file=scenario_file, rendering=True)
+
+
