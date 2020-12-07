86157277465f636c5d82f0c62e9059f4f99913d5
luigiberducci <luigi.berd@gmail.com>
2020-12-07 10:37:01 +0100

    reset max velocity = 5, remove debug print in simple gap follower

--- examples/simple_gap_follower.py
+++ examples/simple_gap_follower.py
@@ -2,6 +2,7 @@ from agents.gap_follower import GapFollower
 from time import sleep
 from racecar_gym import MultiAgentScenario
 from racecar_gym.envs import MultiAgentRaceEnv
+import numpy as np
 
 scenario = MultiAgentScenario.from_spec("custom.yml", rendering=True)
 env = MultiAgentRaceEnv(scenario=scenario)
@@ -15,7 +16,6 @@ while not done:
     action_gf = agent.action(obs['A'])
     action['A'] = {'motor': action_gf[0], 'steering': action_gf[1]}
     obs, rewards, dones, states = env.step(action)
-    print(obs['A']['progress'])
     done = any(dones.values())
     sleep(0.01)
     if t % 10 == 0:
