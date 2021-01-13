0090d4321903c7f99a11d1b576f3773e52b41886
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-01-13 17:48:13 +0100

    refactoring subprocess envs

--- examples/vectorized_example.py
+++ examples/vectorized_example.py
@@ -1,11 +1,11 @@
-from racecar_gym import MultiAgentScenario
+from racecar_gym import MultiAgentScenario, VectorizedSingleAgentRaceEnv, SingleAgentScenario
 from racecar_gym.envs.vectorized_multi_agent_race import VectorizedMultiAgentRaceEnv
 from agents.gap_follower import GapFollower
 
 n_parallel_instances = 2
 rendering = True
-scenarios = [MultiAgentScenario.from_spec('custom.yml', rendering=rendering) for _ in range(n_parallel_instances)]
-env = VectorizedMultiAgentRaceEnv(scenarios=scenarios)
+scenarios = [SingleAgentScenario.from_spec('custom.yml', rendering=rendering) for _ in range(n_parallel_instances)]
+env = VectorizedSingleAgentRaceEnv(scenarios=scenarios)
 n_agents_per_instance = [len(act.spaces.keys()) for act in env.action_space]
 gfollow = GapFollower()
 
@@ -14,17 +14,10 @@ for i in range(3):
     obs = env.reset()
     episode = []
     while not done:
-        action = []
-        for i in range(n_parallel_instances):
-            multi_action = dict()
-            for agent_id in obs[i].keys():
-                act = gfollow.action(obs[i][agent_id])
-                act = {'motor': act[0], 'steering': act[1]}
-                multi_action[agent_id] = act
-            action.append(multi_action)
+        action = env.action_space.sample()
         #action = env.action_space.sample()
         obs, rewards, dones, states = env.step(action)
-        done = any([any(e.values()) for e in dones])
+        done = any([e for e in dones])
         renderings = env.render()
         episode.append(obs)
 env.close()
--- racecar_gym/envs/variable_track_multi_agent_race.py
+++ racecar_gym/envs/variable_track_multi_agent_race.py
@@ -5,47 +5,49 @@ from typing import List, Tuple, Dict
 import gym
 from multiprocessing import Pipe, Process
 from multiprocessing.connection import Connection
+
+from gym import Env
+
 from .scenarios import MultiAgentScenario
 from .multi_agent_race import MultiAgentRaceEnv
+from .subprocess_utils import SubprocessEnv
 from .vectorized_multi_agent_race import VectorizedMultiAgentRaceEnv
 
 
-class DynamicTrackMultiAgentRaceEnv(VectorizedMultiAgentRaceEnv):
+class DynamicTrackMultiAgentRaceEnv(Env):
 
     def __init__(self, scenarios: List[MultiAgentScenario], order: str = 'sequential'):
-        super().__init__(scenarios)
-        assert all([self.observation_space.spaces[0] == space for space in self.observation_space.spaces])
-        assert all([self.action_space.spaces[0] == space for space in self.action_space.spaces])
+        super().__init__()
+
         self._current_track_index = 0
         if order == 'sequential':
             self._order_fn = lambda: (self._current_track_index + 1) % len(scenarios)
         elif order == 'random':
             self._order_fn = lambda: random.choice(list(set(range(0, len(scenarios))) - {self._current_track_index}))
-        self.observation_space = self.observation_space.spaces[0]
-        self.action_space = self.action_space.spaces[0]
+
+        self._envs = [
+            SubprocessEnv(factory=lambda: MultiAgentRaceEnv(scenario=scenario), blocking=True)
+            for scenario
+            in scenarios
+        ]
+        assert all(self._envs[0].action_space == env.action_space for env in self._envs)
+        assert all(self._envs[0].observation_space == env.observation_space for env in self._envs)
+        self.action_space = self._envs[0].action_space
+        self.observation_space = self._envs[0].observation_space
+
 
     def step(self, action: Dict):
-        connection = self._env_connections[self._current_track_index]
-        connection.send('step')
-        connection.send(action)
-        obs, reward, done, state = connection.recv()
-        return obs, reward, done, state
+        return self._get_env().step(action=action)
 
     def reset(self, mode: str = 'grid'):
-        next_track = self._order_fn()
-        connection = self._env_connections[next_track]
-        connection.send('reset')
-        connection.send(mode)
-        obs = connection.recv()
-        self._current_track_index = next_track
-        return obs
+        self._current_track_index = self._order_fn()
+        return self._get_env().reset(mode=mode)
 
     def render(self, mode='follow', agent: str = None, **kwargs):
-        connection = self._env_connections[self._current_track_index]
-        connection.send('render')
-        connection.send((mode, agent, kwargs))
-        image = connection.recv()
-        return image
+        return self._get_env().render(mode=mode, agent=agent, **kwargs)
+
+    def _get_env(self):
+        return self._envs[self._current_track_index]
 
 
 
