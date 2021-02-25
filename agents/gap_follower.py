1889f01939b675f586d63bc1f083238496c027d6
luigiberducci <luigi.berd@gmail.com>
2021-02-25 16:10:46 +0100

    add adaptive control to gfollower, add treitl_v3 scenario

--- agents/gap_follower.py
+++ agents/gap_follower.py
@@ -6,8 +6,10 @@ from scipy.signal import medfilt
 
 class GapFollower:
 
-    def __init__(self):
+    def __init__(self, fixed_speed=True):
         self.threshold = 0.1
+        self._max_range = 15.0
+        self._fixed_speed = fixed_speed
 
     def preprocess_lidar(self, ranges, kernel_size=5):
         # Step 1: interpolate nan values
@@ -17,7 +19,6 @@ class GapFollower:
         x = np.where(~nans)[0]
         y = proc_ranges[~nans]
         proc_ranges[nan_idx] = np.interp(nan_idx, x, y)
-
         # Step 2: apply a median filter to the interpolated values
         proc_ranges = medfilt(proc_ranges, kernel_size)
         return proc_ranges
@@ -33,13 +34,28 @@ class GapFollower:
         else:
             return np.ndarray([0, free_space_ranges.size - 1])
 
-    def find_best_point(self, start_i, end_i, ranges):
+
+    def find_best_point(self, start_i, end_i, ranges, version="center"):
         """Start_i & end_i are start and end indicies of max-gap range, respectively
         Return index of best point in ranges
-        Naive: Choose the furthest point within ranges and go there
         """
-        center_index = int((start_i + end_i) / 2)
-        return center_index
+        if version=="center":
+            target = int((start_i + end_i) / 2)
+        elif version=="demo":
+            furthest = np.argmax(ranges[start_i:end_i]) + start_i
+            center = (end_i + start_i) / 2
+            factor = 0.5
+            target = int(np.round(factor * furthest + (1.0 - factor) * center))
+        else:
+            target = 0.0
+        return target
+
+    def _get_motor(self, angle, max_range):
+        """ Compute the motor force based on the maximum frontal range """
+        if self._fixed_speed or max_range < 2.5:
+            return 0.01
+        else:
+            return max_range / self._max_range * 0.5
 
     def action(self, observation: Dict[str, np.ndarray]) -> Tuple[float, float]:
         scan = observation['lidar']
@@ -60,15 +76,15 @@ class GapFollower:
         if len(gap) < 1:
             return (0, 0)
         # Find the best point in the gap
-        best_point = min_index + self.find_best_point(start_i=gap[0], end_i=gap[1], ranges=proc_ranges)
+        best_point = min_index + self.find_best_point(start_i=gap[0], end_i=gap[1], ranges=proc_ranges, version="demo")
 
         # Publish Drive message
         angle = (-math.pi/2 + best_point * math.pi / 1080)
         angle = math.copysign(min(1, abs(angle)), angle)
 
-        command = angle / 0.42
-
-        return np.random.normal(loc=0.01, scale=0.0), np.random.normal(loc=command, scale=0.0)
+        steering = np.clip(angle * 2, -1, +1)
+        motor = self._get_motor(angle, max(proc_ranges[1080//2-50:1080//2+50]))
+        return np.random.normal(loc=motor, scale=0.0), np.random.normal(loc=steering, scale=0.0)
 
     def __call__(self, obs, *args):
         return np.expand_dims(np.array(self.action(obs)), 0), obs
