b60b3456b355f3b3126e17afbe0210e6edb4347c
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2022-05-02 10:31:00 +0200

    Specifying dependency versions. Fixes issue #7.

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setup(
     name="racecar_gym",
     version="0.1",
     packages=find_packages(),
-    install_requires=['pybullet', 'scipy', 'numpy', 'gym', 'yamldataclassconfig', 'nptyping', 'pettingzoo'],
+    install_requires=['pybullet==3.1.7',
+                      'scipy', # version automatically choosen w.r.t numpy
+                      'numpy==1.22.3',
+                      'gym==0.21.0',
+                      'yamldataclassconfig==1.5.0',
+                      'nptyping<2.0',
+                      'pettingzoo==1.18.1'
+                      ],
     author='Axel Brunnbauer',
     author_email='axel.brunnbauer@gmx.at',
     description='An RL environment for a miniature racecar using the pybullet physics engine.',
