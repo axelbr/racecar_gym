ed2eca3c8adfee21e12a674c38b8c5136eda7231
Luigi Berducci <luigi.berd@gmail.com>
2022-07-04 18:00:56 +0200

    bugfix: raycasting was ignoring walls closer than `mindist` from sensor, causing to look beyond the walls
    (cherry picked from commit d2fb72b2d78ad88622639e58aa71e08eb547945e)

--- racecar_gym/bullet/sensors.py
+++ racecar_gym/bullet/sensors.py
@@ -83,8 +83,10 @@ class Lidar(BulletSensor[NDArray[(Any,), np.float]]):
                                                    rays=self._rays)
 
     def _setup_raycast(self, min_distance: float, scan_range: float, rays: int):
-        start = min_distance
-        end = min_distance + scan_range
+        # bugfix: the raycast must start from the car,
+        # otherwise it will ignore walls that closer than  ̀mindist`from the sensor
+        start = 0.0
+        end = scan_range
         from_points, to_points = [], []
         angle = self._config.angle_start + np.pi / 2.0
         increment = self._config.angle / self._config.rays
