bef122663d6fca13297b177546420ea5e908e4cf
Luigi Berducci <luigi.berd@gmail.com>
2022-07-05 18:34:37 +0200

    Feature: add speed actuator (#9)
    * add speed actuator,
    update parameters to have a max speed of 3.5 ms
    
    * actuators: add 'speed' actuator, extend providers and specs to support actuators' selection
    
    * modify custom.yml for actuator selection of agent A
    
    (cherry picked from commit 93fa0427055de99fd0f38f036d4670b2e3ea54fd)
    
    * set actuators typo to np.float32 to avoid casting errors
    
    (cherry picked from commit 4e64728f1ab03733410101d95055fbc0cdb8d2ff)

--- racecar_gym/bullet/actuators.py
+++ racecar_gym/bullet/actuators.py
@@ -59,7 +59,38 @@ class Motor(BulletActuator[Tuple[float, float]]):
             )
 
     def space(self) -> gym.Space:
-        return gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float64)
+        return gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
+
+
+class Speed(BulletActuator[Tuple[float, float]]):
+    @dataclass
+    class Config:
+        velocity_multiplier: float
+        max_velocity: float
+        max_force: float
+
+    def __init__(self, name: str, config: Config):
+        super().__init__(name)
+        self._config = config
+
+    def control(self, target_speed: float) -> None:
+        """ target_speed is assumed to be mapped from [0,max_velocity] to [-1, +1]"""
+        target_speed = np.clip(target_speed, -1, +1)  # sanity check
+        target_speed = (target_speed + 1.0) / 2.0 * self._config.max_velocity  # convert to actual range
+
+        velocity = target_speed * self._config.velocity_multiplier
+        force = self._config.max_force
+
+        for index in self.joint_indices:
+            pybullet.setJointMotorControl2(
+                self.body_id, index,
+                pybullet.VELOCITY_CONTROL,
+                targetVelocity=velocity,
+                force=force
+            )
+
+    def space(self) -> gym.Space:
+        return gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
 
 
 class SteeringWheel(BulletActuator[float]):
@@ -83,4 +114,4 @@ class SteeringWheel(BulletActuator[float]):
             )
 
     def space(self) -> gym.Space:
-        return gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float64)
+        return gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
